40d211ed4f83069674714f4ea607d9fd39e850db
IndomieGorengSatu <96320576+IndomieGorengSatu@users.noreply.github.com>
2023-05-25 22:17:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,6 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
@@ -61,7 +60,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(1447438514)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
